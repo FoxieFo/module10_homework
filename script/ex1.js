6f2725719f089c43058b8d90e9aaaab66b1087b0
Victoria <151223562+FoxieFo@users.noreply.github.com>
2024-03-02 03:25:46 +0100

    Update ex1.js

--- script/ex1.js
+++ script/ex1.js
@@ -1,6 +1,3 @@
-// Задание 1.
-// Сверстайте кнопку, которая будет содержать в себе icon_01 (как в примере в последнем видео). При клике на кнопку иконка должна меняться на icon_02. Повторный клик меняет иконку обратно.
-
 const btn = document.querySelector('.j-btn');
 const sparkle = document.querySelector('.sparkle');
 const btnIcon = document.querySelector('.btn__icon')
